3eb9deb1f54946a613fff5628ac126feee745d50
Mateus Da Silva <mateusds.mds@gmail.com>
2020-12-02 15:14:26 -0500

    Initial Commit

---
+++ BasicTabs/BasicTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicTabsApp.swift
+//  BasicTabs
+//
+//  Created by Mateus Da Silva on 12/2/20.
+//
+
+import SwiftUI
+
+@main
+struct BasicTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
